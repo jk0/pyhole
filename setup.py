3bd04bd59ecbcf03aed7b3255fd295dc4751805b
Sandy Walsh <sandy.walsh@rackspace.com>
2011-03-04 08:51:19 -0400

    started setup - broken now

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requirements = ['eventlet', 'IMDbPY', 'pywapi', 'simplejson']
+if sys.version_info < (2,6):
+    requirements.append('simplejson')
+
+setup(
+    name = "pyhole",
+    version = "0.5.1",
+    description = "Modular IRC Bot for Python",
+    long_description = read('README.rst'),
+    url = 'https://github.com/jk0/pyhole',
+    license = 'Apache',
+    author = 'Josh Kearney',
+    author_email = 'josh@jk0.org',
+    packages = find_packages(exclude=['tests']),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Service',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = requirements,
+    
+    entry_points = {
+        'console_scripts': ['pyhole = pyhole.py']
+    }
+)
