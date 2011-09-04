44faefbd967d16b3cd4ced0d0dfb1231c9257a9d
Paul Voccio <paul@substation9.com>
2011-09-04 17:26:39 -0500

    fixing typo

--- plugins/chat.py
+++ plugins/chat.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-"""Pyhole Launchpad Plugin"""
+"""Pyhole Cleverbot Plugin"""
 
 import cleverbot
 
