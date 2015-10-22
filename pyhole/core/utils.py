dcefa0f2feb46fe8d85d8a4d5e16e2103b80b027
Josh Kearney <josh@jk0.org>
2015-10-22 16:20:02 -0500

    Update default config.

--- pyhole/core/utils.py
+++ pyhole/core/utils.py
@@ -220,12 +220,6 @@ domain: jira.example.com
 username: abcd1234
 password: pass1234
 
-[VersionOne]
-domain: www1.v1host.com
-key: EXAMPL
-username: user
-password: abcd1234
-
 [Xsa]
 notify: #channel1, #channel2
 
